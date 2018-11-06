b518a57b8037ca255f884756ee7cf2b62ceebb45
dzzzzzy <insistyao_0522@163.com>
2018-11-06 20:21:57 +0800

    chore(service): refactor base class

--- src/services/wechat.pay.service.ts
+++ src/services/wechat.pay.service.ts
@@ -1,42 +1,49 @@
-import { HttpService, Inject, Injectable } from '@nestjs/common';
+import { HttpService, Inject, Injectable, OnModuleInit } from '@nestjs/common';
 
 import { PayAddonConfigProvider } from '../constants/addon.constant';
 import { PayAddonConfig } from '../interfaces/addon.config.interface';
+import { SandboxResponse } from '../interfaces/sandbox.interface';
 import { RandomUtil } from '../utils/random.util';
 import { SignUtil } from '../utils/sign.util';
 import { XmlUtil } from '../utils/xml.util';
 
 @Injectable()
-export class WechatPayService {
+export class WechatPayService implements OnModuleInit {
     /** API 接口域名 */
     private apiBase = 'https://api.mch.weixin.qq.com';
     /** 统一下单接口地址 */
-    private readonly unifiedOrderUrl = `${this.apiBase}/pay/unifiedorder`;
+    protected readonly unifiedOrderUrl = `${this.apiBase}/pay/unifiedorder`;
     /** 查询订单接口地址 */
-    private readonly orderQueryUrl = `${this.apiBase}/pay/orderquery`;
+    protected readonly orderQueryUrl = `${this.apiBase}/pay/orderquery`;
     /** 关闭订单接口地址 */
-    private readonly closeOrderUrl = `${this.apiBase}/pay/closeorder`;
+    protected readonly closeOrderUrl = `${this.apiBase}/pay/closeorder`;
     /** 申请退款接口地址 */
-    private readonly refundUrl = `${this.apiBase}/secapi/pay/refund`;
+    protected readonly refundUrl = `${this.apiBase}/secapi/pay/refund`;
     /** 查询退款接口地址 */
-    private readonly refundQueryUrl = `${this.apiBase}/pay/refundquery`;
+    protected readonly refundQueryUrl = `${this.apiBase}/pay/refundquery`;
     /** 下载对账单接口地址 */
-    private readonly downloadBillUrl = `${this.apiBase}/pay/downloadbill`;
+    protected readonly downloadBillUrl = `${this.apiBase}/pay/downloadbill`;
     /** 下载资金账单接口地址 */
-    private readonly downloadFundFlowUrl = `${this.apiBase}/pay/downloadfundflow`;
+    protected readonly downloadFundFlowUrl = `${this.apiBase}/pay/downloadfundflow`;
     /** 沙箱环境获取验签秘钥接口地址 */
-    private readonly sandboxGetSignKeyUrl = 'https://api.mch.weixin.qq.com/sandboxnew/pay/getsignkey';
+    protected readonly sandboxGetSignKeyUrl = 'https://api.mch.weixin.qq.com/sandboxnew/pay/getsignkey';
 
     constructor(
-        @Inject(HttpService) private readonly httpService: HttpService,
-        @Inject(PayAddonConfigProvider) private readonly payAddonConfig: PayAddonConfig,
-        @Inject(XmlUtil) private readonly xmlUtil: XmlUtil,
-        @Inject(SignUtil) private readonly signUtil: SignUtil,
-        @Inject(RandomUtil) private readonly randomUtil: RandomUtil
+        @Inject(HttpService) protected readonly httpService: HttpService,
+        @Inject(PayAddonConfigProvider) protected readonly payAddonConfig: PayAddonConfig,
+        @Inject(XmlUtil) protected readonly xmlUtil: XmlUtil,
+        @Inject(SignUtil) protected readonly signUtil: SignUtil,
+        @Inject(RandomUtil) protected readonly randomUtil: RandomUtil
     ) {
         this.payAddonConfig.sandbox && (this.apiBase += '/sandbox');
-        // 缓存沙箱秘钥 sandbox_signkey
-        this.getSandboxSignKey();
+    }
+
+    async onModuleInit() {
+        const data = await this.getSandboxSignKey();
+        if (data.return_code === 'FAIL') {
+            throw new Error(data.retmsg);
+        }
+        this.payAddonConfig.secretKey = data.sandbox_signkey;
     }
 
     /**
@@ -49,7 +56,7 @@ export class WechatPayService {
             sign: ''
         };
         params.sign = await this.signUtil.wechatSign(params, this.payAddonConfig.secretKey);
-        const { data } = await this.httpService.post(this.sandboxGetSignKeyUrl, this.xmlUtil.convertObjToXml(params)).toPromise();
-        return await this.xmlUtil.parseObjFromXml(data);
+        const { data } = await this.httpService.post<SandboxResponse>(this.sandboxGetSignKeyUrl, this.xmlUtil.convertObjToXml(params)).toPromise();
+        return await this.xmlUtil.parseObjFromXml<SandboxResponse>(data);
     }
 }
