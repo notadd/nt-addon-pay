3e62bcbc1e93cadd006cdf6679008143b3ae197a
dzzzzzy <insistyao_0522@163.com>
2018-11-07 13:00:02 +0800

    refactor(util): refactor sign util

--- src/utils/sign.util.ts
+++ src/utils/sign.util.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import * as crypto from 'crypto';
 
+/**
+ * 签名工具
+ */
 @Injectable()
 export class SignUtil {
     /**
@@ -8,16 +11,16 @@ export class SignUtil {
      *
      * @param params 参数
      * @param secretKey 秘钥
-     * @param hashType 签名方式(选填)，默认md5
+     * @param hashType 签名方式(选填)，默认MD5
      */
-    wechatSign(params: {}, secretKey: string, hashType?: 'md5' | 'sha256') {
+    wechatSign(params: {}, secretKey: string, hashType?: 'MD5' | 'HMAC-SHA256') {
         const paramArr: string[] = [];
         const sortedKeys = Object.keys(params).sort();
         for (const key of sortedKeys) {
             params[key] && paramArr.push(`${key}=${params[key]}`);
         }
         let signStr = paramArr.join('&');
-        if (hashType && hashType === 'sha256') {
+        if (hashType && hashType === 'HMAC-SHA256') {
             return crypto.createHmac('sha256', secretKey).update(signStr).digest('hex').toUpperCase();
         }
         return crypto.createHash('md5').update(signStr += `&key=${secretKey}`).digest('hex').toUpperCase();
