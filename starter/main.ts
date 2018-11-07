9cafc82e929208d8173aa49dd8eeebbd729e11c3
dzzzzzy <insistyao_0522@163.com>
2018-11-07 13:01:19 +0800

    refactor(starter): refactor starter

--- starter/main.ts
+++ starter/main.ts
@@ -4,10 +4,12 @@ import { PayAddon } from '../src/pay.addon';
 
 async function bootstrap() {
     const app = await NestFactory.create(PayAddon.forRoot({
-        appid: 'appid',
-        mch_id: 'mch_id',
-        secretKey: 'secretKey',
-        sandbox: true
+        wechatConfig: {
+            appid: 'appid',
+            mch_id: 'mch_id',
+            secretKey: 'secretKey',
+            sandbox: true
+        }
     }));
     await app.listen(3000);
 }
