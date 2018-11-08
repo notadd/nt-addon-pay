6030bb0e46b05d05e8b8a3dbac774fc14fb51e71
dzzzzzy <insistyao_0522@163.com>
2018-11-08 18:51:45 +0800

    feat(wechat): add wechat base query and close order logic

--- src/modules/wechat/services/wechat.pay.base.service.ts
+++ src/modules/wechat/services/wechat.pay.base.service.ts
@@ -1,6 +1,12 @@
 import { Inject, Injectable } from '@nestjs/common';
 
 import { PayAddonConfig, PayAddonConfigProvider } from '../../../common';
+import {
+    WechatBaseCloseOrderReqParam,
+    WechatBaseCloseOrderRes,
+    WechatBaseQueryOrderReqParam,
+    WechatBaseQueryOrderRes,
+} from '../interfaces/order.interface';
 import { WechatRequestUtil } from '../utils/request.util';
 
 
@@ -11,7 +17,7 @@ export class WechatPayBaseService {
     /** 统一下单接口地址 */
     protected readonly unifiedOrderUrl = `${this.apiBase}/pay/unifiedorder`;
     /** 查询订单接口地址 */
-    protected readonly orderQueryUrl = `${this.apiBase}/pay/orderquery`;
+    protected readonly queryOrderUrl = `${this.apiBase}/pay/orderquery`;
     /** 关闭订单接口地址 */
     protected readonly closeOrderUrl = `${this.apiBase}/pay/closeorder`;
     /** 申请退款接口地址 */
@@ -27,4 +33,34 @@ export class WechatPayBaseService {
         @Inject(PayAddonConfigProvider) private readonly payAddonConfig: PayAddonConfig,
         @Inject(WechatRequestUtil) protected readonly requestUtil: WechatRequestUtil
     ) { }
+
+    /**
+     * 查询订单
+     *
+     * @param params 查询订单请求参数
+     */
+    protected async queryOrder(params: WechatBaseQueryOrderReqParam): Promise<WechatBaseQueryOrderRes> {
+        this.checkOverrideDefaultSignType(params);
+        return await this.requestUtil.post<WechatBaseQueryOrderRes>(this.queryOrderUrl, params);
+    }
+
+    /**
+     * 关闭订单
+     *
+     * @param params 关闭订单请求参数
+     */
+    protected async closeOrder(params: WechatBaseCloseOrderReqParam): Promise<WechatBaseCloseOrderRes> {
+        this.checkOverrideDefaultSignType(params);
+        return await this.requestUtil.post<WechatBaseCloseOrderRes>(this.closeOrderUrl, params);
+    }
+
+    /**
+     * 检查是否覆盖默认的签名类型
+     */
+    private checkOverrideDefaultSignType(params: any) {
+        const signType = this.payAddonConfig.wechatConfig.sign_type;
+        if (signType) {
+            (params as any).sign_type = signType;
+        }
+    }
 }
