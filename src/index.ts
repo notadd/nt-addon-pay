bc37cc5b423b4bd53630b5b8fe93840a974fccda
dzzzzzy <insistyao_0522@163.com>
2018-11-22 15:07:44 +0800

    chore(*): add export

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export * from './pay.addon';
+export * from './modules/wechat/services/app.pay.service';
+export * from './modules/wechat/services/applet.pay.service';
+export * from './modules/wechat/services/jsapi.pay.service';
+export * from './modules/wechat/services/micro.pay.service';
+export * from './modules/wechat/services/native.pay.service';
+export * from './modules/wechat/services/wap.pay.service';
