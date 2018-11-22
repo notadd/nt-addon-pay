3d9182e230e2cdfd0cb3531f75cc7f3ba51f3451
dzzzzzy <insistyao_0522@163.com>
2018-11-22 11:19:55 +0800

    chore(interface): add more comment

--- src/modules/wechat/interfaces/sandbox.interface.ts
+++ src/modules/wechat/interfaces/sandbox.interface.ts
@@ -3,6 +3,7 @@ export interface WechatSandboxResponse {
     return_code: string;
     /** 返回信息 */
     return_msg?: string;
+    /** 返回信息 */
     retmsg?: string;
     /** 沙箱秘钥 */
     sandbox_signkey: string;
