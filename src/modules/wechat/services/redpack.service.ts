f8082770df21fbd89d9df30a0e6b7f67d5f28e05
dzzzzzy <insistyao_0522@163.com>
2018-12-10 11:13:44 +0800

    feat(wechat): add send group redpack api and query redpack record api

--- src/modules/wechat/services/redpack.service.ts
+++ src/modules/wechat/services/redpack.service.ts
@@ -1,6 +1,11 @@
 import { Injectable } from '@nestjs/common';
 
-import { WeChatRedpackReqParam, WeChatRedpackRes } from '../interfaces/redpack.interface';
+import {
+    WeChatQueryRedpackRecordReqParam,
+    WeChatQueryRedpackRecordRes,
+    WeChatRedpackReqParam,
+    WeChatRedpackRes
+} from '../interfaces/redpack.interface';
 import { WeChatPayBaseService } from './base.service';
 
 /**
@@ -20,5 +25,25 @@ export class WeChatRedpackService extends WeChatPayBaseService {
      */
     async sendRedpack(params: WeChatRedpackReqParam): Promise<WeChatRedpackRes> {
         return await this.requestUtil.post<WeChatRedpackRes>(this.sendredpackUrl, params, { httpsAgent: this.certificateAgent });
+    }
+
+    /**
+     * 发放裂变红包
+     *
+     * @param params 发放裂变红包请求参数
+     */
+    async sendGroupRedpack(params: WeChatRedpackReqParam): Promise<WeChatRedpackRes> {
+        (params as any).amt_type = 'ALL_RAND';
+        return await this.requestUtil.post<WeChatRedpackRes>(this.sendgroupredpackUrl, params, { httpsAgent: this.certificateAgent });
+    }
+
+    /**
+     * 查询红包记录
+     *
+     * @param params 查询红包记录请求参数
+     */
+    async queryRedpackRecord(params: WeChatQueryRedpackRecordReqParam): Promise<WeChatQueryRedpackRecordRes> {
+        (params as any).bill_type = 'MCHT';
+        return await this.requestUtil.post<WeChatQueryRedpackRecordRes>(this.gethbinfoUrl, params, { httpsAgent: this.certificateAgent });
     }
 }
