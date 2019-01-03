3e850d0769c77090539bfea5a219a4c5abbd55dc
dzzzzzy <insistyao_0522@163.com>
2019-01-03 10:29:15 +0800

    chore(random): use nanoid to generate random string

--- src/shared/utils/random.util.ts
+++ src/shared/utils/random.util.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import * as chance from 'chance';
+import nanoid from 'nanoid/async/generate';
 
 /**
  * 随机数工具
@@ -8,8 +8,21 @@ import * as chance from 'chance';
 export class RandomUtil {
     /**
      * 生成32位随机字符串
+     *
+     * @param pool 随机字符池，默认：ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789
+     *
+     * @param length 随机字符长度，默认：32
      */
-    genRandomStr() {
-        return chance().string({ length: 32, pool: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789' });
+    async genRandomStr(pool?: string, length?: number): Promise<string> {
+        if (pool && !length) {
+            return await nanoid(pool, 32);
+        }
+        if (!pool && length) {
+            return await nanoid('ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789', length);
+        }
+        if (pool && length) {
+            return await nanoid(pool, length);
+        }
+        return await nanoid('ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789', 32);
     }
 }
